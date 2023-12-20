b70dba2c465c1ca10c8215245321c63011fb957b
KAKANURUYOGASREEDHARREDDY <103167842+KAKANURUYOGASREEDHARREDDY@users.noreply.github.com>
2023-12-20 18:11:44 +0530

    Create .js

---
+++ .js
@@ -0,0 +1,14 @@
+<!-- Custom JavaScript -->
+<script>
+    // Function to toggle image size on click
+    function toggleImageSize(imageId) {
+        var img = document.getElementById(imageId);
+        if (img.style.height === "200px") {
+            img.style.height = "400px";
+        } else {
+            img.style.height = "200px";
+        }
+    }
+
+    // You can add more JavaScript functions or code here
+</script>
